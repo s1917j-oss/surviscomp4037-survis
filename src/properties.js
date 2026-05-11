ad2bbcbaa9c0bb80fd09948b71d73b1f5f0933c5
s1917j-oss <s1917j@gmail.com>
2026-05-11 04:56:31 +0100

    Update properties.js

--- src/properties.js
+++ src/properties.js
@@ -1,61 +1,74 @@
-var literatureTitle = "Large Language Models for Software Engineering Literature Collection";
+/**
+ * Name as used as title for the main page
+ * @type {string}
+ */
+var title = 'SurVis Literature Collection';
 
+/**
+ * Relative path to the data directory
+ * @type {string}
+ */
+var dataDir = 'data/';
+
+/**
+ * Relative path to the js directory
+ * @type {string}
+ */
+var jsDir = 'js/';
+
+/**
+ * Relative path to the styles directory
+ * @type {string}
+ */
+var stylesDir = 'styles/';
+
+/**
+ * Options for displaying tag clouds.
+ */
+var tagCloudOptions = [
+    {
+        field: 'keywords',
+        title: 'Keywords',
+        minTagFrequency: 1
+    },
+    {
+        field: 'author',
+        title: 'Authors',
+        minTagFrequency: 1
+    },
+    {
+        field: 'series',
+        title: 'Series',
+        minTagFrequency: 1
+    }
+];
+
+/**
+ * If BibTeX entries and tags should be editable
+ */
+var editable = true;
+
+/**
+ * Subtitle describing the paper the data is referring to
+ */
 var paper = {
-    title: "Literature Collection",
-    url: ""
+    html: 'AI Programming Assistants Literature Collection',
+    id: 'Barke2023Grounded'
 };
 
+/**
+ * Extra pages like an about page
+ */
 var extraPages = {
+    'about': 'about.html'
 };
 
-var editable = false;
+/**
+ * Custom style as path to an extra css file
+ */
+var customStyle = '';
 
-var showSelectButton = false;
-
-var showTagCloud = true;
-
-var showTimeline = true;
-
-var showClusters = true;
-
-var showBibtex = true;
-
-var showCitation = true;
-
-var showPdf = false;
-
-var showThumbnail = false;
-
-var showAbstract = true;
-
-var showReferences = false;
-
-var showCitationCount = false;
-
-var tagCategories = {
-};
-
-var specialTags = {
-};
-
-var tagLabels = {
-};
-
-var tagColors = {
-};
-
-var tagDisplay = {
-};
-
-var tagDefaultFilter = {
-};
-
-var authorDisplay = {
-};
-
-var seriesDisplay = {
-};
-
-var cssFile = "";
-
-var customStyle = "";
+/**
+ * Options for showing citation information
+ */
+var citations = null;
